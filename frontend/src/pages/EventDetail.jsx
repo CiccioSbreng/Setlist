f954e4e4ab2c69c27a2a9f29f36662719e40a5cc
CiccioSbreng <boc_flub@hotmail.it>
2026-05-17 09:36:33 +0000

    Rinominati bottoni mappa: 'Vedi su Maps' e 'Indicazioni'

--- frontend/src/pages/EventDetail.jsx
+++ frontend/src/pages/EventDetail.jsx
@@ -536,8 +536,8 @@ export default function EventDetail() {
                 <div className="ed-tile__head">
                   <PinIcon size={16} /><span>Come arrivare</span>
                   <div className="ed-tile__links">
-                    <a href={gmaps} target="_blank" rel="noreferrer" className="btn btn--outline btn--sm">Maps</a>
-                    <a href={gdir} target="_blank" rel="noreferrer" className="btn btn--outline btn--sm">Vai</a>
+                    <a href={gmaps} target="_blank" rel="noreferrer" className="btn btn--outline btn--sm">Vedi su Maps</a>
+                    <a href={gdir} target="_blank" rel="noreferrer" className="btn btn--outline btn--sm">Indicazioni</a>
                   </div>
                 </div>
                 {showMap ? (
